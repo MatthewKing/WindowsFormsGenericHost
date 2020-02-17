3e84e5b35b9d4e6d350e151157aa284522876054
Matthew King <mking@mking.net>
2020-02-17 16:38:01 +0800

    Add sample/demo project.

---
+++ samples/WindowsFormsGenericHost.Demo/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsGenericHost.Demo
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ samples/WindowsFormsGenericHost.Demo/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WindowsFormsGenericHost.Demo
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            using var host = Host.CreateDefaultBuilder()
+                .ConfigureServices(ConfigureServices)
+                .UseWindowsFormsLifetime<MainForm>()
+                .Build();
+
+            host.Run();
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<MainForm>();
+        }
+    }
+}
